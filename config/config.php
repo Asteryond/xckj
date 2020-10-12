88c5262e01158eca2873400df444c69c7f784300
aster <2109049270@qq.com>
2020-10-12 22:51:34 +0800

    添加主界面

--- config/config.php
+++ config/config.php
@@ -5,6 +5,6 @@ $config['db']['password'] = '220125';
 $config['db']['dbname'] = 'xckj';
 //$config['db']['host']  = 3306;
 
-$config['defaultController'] = 'main';
+$config['defaultController'] = 'Main';
 $config['defaultAction'] = 'index';
 return $config;
